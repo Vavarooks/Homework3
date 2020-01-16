6ecb0a664770dad70824a97e44e095493b2bfdf0
Vavarooks <vavarooks123@gmail.com>
2020-01-15 18:14:43 -0800

    Java and HTML edit

--- Back.js
+++ Back.js
@@ -1,6 +1,6 @@
 // Assignment Code
 
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
@@ -8,7 +8,7 @@ var generateBtn = document.querySelector("#generate");
 
 function makeid(length) {
   var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
   var charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
      result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -17,17 +17,14 @@ function makeid(length) {
 }
 
 
-let pass = confirm("Do you want to generate a password?");
+// let passWord = confirm("Do you want to generate a password?");
 
-if (pass) {
-  alert(makeid(8));
-}
-else {
-  console.log(makeid(8));
-}
-
-
-// function myFunction() {
-//   var x = document.getElementById("password").value;
-//   document.getElementById("demo").innerHTML = x;
+// if (passWord) {
+//   alert(makeid(8));
 // }
+// else {
+//   console.log(makeid(8));
+// }
+
+
+
