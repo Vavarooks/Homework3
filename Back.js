2b549bbf1e703bf59d77b0bb35c0388a785cd7eb
Vavarooks <vavarooks123@gmail.com>
2020-01-15 19:29:47 -0800

    Css, Java, and HTML edits and additons

--- Back.js
+++ Back.js
@@ -2,20 +2,17 @@
 
 // var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
-
 
 function makeid(length) {
   var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  var characters       = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&'()*+,-./:;<=>?@[]^_{|}~`;
   var charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
      result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
-
 
 // let passWord = confirm("Do you want to generate a password?");
 
